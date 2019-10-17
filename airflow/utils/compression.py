6e6bc4cc844c106513a20362f13827c40dc0b23d
kik-kik <kik@medicuja.com>
2019-10-17 12:00:50 +0200

    airflow.utils.compression.py pylint compliant

--- airflow/utils/compression.py
+++ airflow/utils/compression.py
@@ -16,6 +16,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""Module containing compression helper functions"""
 
 import bz2
 import gzip
